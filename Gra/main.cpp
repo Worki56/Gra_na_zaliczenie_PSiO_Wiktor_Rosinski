8d0be2b5c788bafa6a1c5b63e1937cf2e28d130e
Worki56 <w-r125@wp.pl>
2020-05-25 13:43:26 +0200

    1

---
+++ Gra/main.cpp
@@ -0,0 +1,28 @@
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <memory>
+#include <math.h>
+#include <fstream>
+#include <sstream>
+
+#define GL_SILENCE_DEPRECATION
+
+#include <SFML/OpenGL.hpp>
+
+#ifdef __APPLE__
+#include <OpenGL/glu.h> // macOS
+#else
+#include <GL/glu.h> // Windows/Linux
+#endif
+
+int main() {
+
+
+    return 0;
+}
+
